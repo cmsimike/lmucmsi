a85e5babd783132b449f3986f4a35456c397d54c
Mike Megally <cmsimike@gmail.com>
2014-08-25 18:41:16 -0700

    maven debugging

--- MavenExample/src/test/java/edu/lmu/cmsi/mike/mavenexample/LameSubtractTest.java
+++ MavenExample/src/test/java/edu/lmu/cmsi/mike/mavenexample/LameSubtractTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-public class TestLameSubtract {
+public class LameSubtractTest {
 
       @Test
     public void subtractGood() {
