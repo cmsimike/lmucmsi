698d3464df7a2115172ac51c213b1c51817afc0f
Mike Megally <cmsimike@gmail.com>
2014-09-02 19:02:35 -0700

    TABS!!

--- MikeVsGoogle/ForLoopRunner.java
+++ MikeVsGoogle/ForLoopRunner.java
@@ -7,18 +7,18 @@
  */
 public class ForLoopRunner {
 
-	public static void main(String[] args) {
-		long current = System.currentTimeMillis();
-		int a = 1;
-		for(int i = Integer.MIN_VALUE; i < Integer.MAX_VALUE; i++) {
-			if(a % 2 == 0) {
-				a = 1;
-			}
-			else {
-				a++;
-			}
-		}
+    public static void main(String[] args) {
+        long current = System.currentTimeMillis();
+        int a = 1;
+        for(int i = Integer.MIN_VALUE; i < Integer.MAX_VALUE; i++) {
+            if(a % 2 == 0) {
+                a = 1;
+            }
+            else {
+                a++;
+            }
+        }
 
-		System.out.println((System.currentTimeMillis() - current)/1000 + " seconds");
-	}
+        System.out.println((System.currentTimeMillis() - current)/1000 + " seconds");
+    }
 }
